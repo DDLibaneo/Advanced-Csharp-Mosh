765803ea5004e6833e20698e3059b7adfc47ed62
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-11-28 23:15:11 -0300

    add project EventsAndDelegates

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() 
+            { 
+                Title = "Video" 
+            };
+
+            var videoEncoder = new VideoEncoder();
+
+            videoEncoder.Encode(video);
+
+            Console.WriteLine("Press a key to close...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace EventsAndDelegates
+{
+    internal class VideoEncoder
+    {
+        public VideoEncoder() { }
+
+        public void Encode(Video video)
+        {
+            System.Console.WriteLine("Encoding video...");
+            Thread.Sleep(2000);
+        }
+    }
+}
