b443b485a6e6f18a3640a33c27230fc166cbe147
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-10 23:08:12 -0300

    add comments

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,7 +6,14 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string post = "This is supposed to be a very long blog post bla bla bla...";
+
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+
+            Console.WriteLine("Press a key to close...");
+            Console.ReadKey();
         }
     }
 }
