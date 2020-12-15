82084c29ce8fc932c891708b1019f3172165fd4e
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-14 21:04:31 -0300

    initial commit

---
+++ LINQ-Study/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
