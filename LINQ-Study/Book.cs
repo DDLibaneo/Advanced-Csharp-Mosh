dcafcd9d8dcebe6c41edab033b226119cd1969bd
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-14 21:30:33 -0300

    add exercise classes

---
+++ LINQ-Study/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Study
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public float Price { get; set; }
+    }
+}
