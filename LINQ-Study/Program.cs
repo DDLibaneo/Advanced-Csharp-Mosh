5e2ef574717f8e3879701a2acb171b7679d3e371
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-01-11 17:34:15 -0300

    add mais exemplos de metodos de extensao Linq

--- LINQ-Study/Program.cs
+++ LINQ-Study/Program.cs
@@ -15,12 +15,12 @@ namespace LINQ_Study
                 .Where(b => b.Price < 10)
                 .OrderBy(b => b.Title);
 
-            Console.WriteLine("Exemplo 1\n");
+            Console.WriteLine("Exemplo 1: Linq basico\n");
 
             foreach (var book in cheapBooks)
                 Console.WriteLine(book.Title + " $" + book.Price);
 
-            Console.WriteLine("\nExemplo 2\n");
+            Console.WriteLine("\nExemplo 2: Metodo de extensao Linq Select()\n");
 
             // Sobre o Select()
             // Select é usado para projeções e transformações. P.e, se vc 
@@ -31,7 +31,7 @@ namespace LINQ_Study
             foreach (var title in cheapBooksTitles)
                 Console.WriteLine(title);
 
-            Console.WriteLine("\nExemplo 3\n");
+            Console.WriteLine("\nExemplo 3: Linq com Query Operators (from, where, select, etc)\n");
 
             // LINQ Query Operators
             var cheapBooksQueryOperator = 
@@ -42,6 +42,23 @@ namespace LINQ_Study
 
             foreach (var book in cheapBooksQueryOperator)
                 Console.WriteLine(book.Title);
+
+            Console.WriteLine("\nExemplo 4: Metodos de extensao Linq interessantes");
+            Console.WriteLine("Paginacao\n");
+            // Exemplos de métodos de extensão Linq interessantes
+            // Paginação
+            var pagedBooks = books
+                .Skip(2)
+                .Take(3);
+            
+            foreach (var book in pagedBooks)
+                Console.WriteLine(book.Title + " - $" + book.Price);
+
+            Console.WriteLine("\nSoma\n");
+            // Soma
+            var somaPrecos = books.Sum(b => b.Price);
+
+            Console.WriteLine("Preço total dos livros: " + somaPrecos);
 
             Console.WriteLine("Press a key to close...");
             Console.ReadKey();
