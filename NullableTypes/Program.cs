5a369bed0061357bf01f0653a40081fcd72fb6fd
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-01-13 18:51:31 -0300

    created new project

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Press a key to close...");
+            Console.ReadKey();
+        }
+    }
+}
