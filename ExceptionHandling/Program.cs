61a9c4296f3337f45b9acfb61a79044379db34ca
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-01-16 19:59:43 -0300

    add new project

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Press a key to close...");
+            Console.ReadKey();
+        }
+    }
+}
