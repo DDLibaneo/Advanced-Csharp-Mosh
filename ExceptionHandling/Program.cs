4eaa0bf96b9eeda3395a2101e7482b4f02463b43
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-01-16 21:15:36 -0300

    add exemplos de multiplos catch e finally

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -6,9 +6,35 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by zero.");
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine("An arithmetic error occurred.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally // Usado geralmente pra fazer Dispose. 
+            {
+                // Exemplos: Conexões de banco, File handlers, network collections, 
+                // graphic handlers.
 
+                // Qualquer classe que usa recursos que não são gerenciadas pelo Garbage 
+                // Collection é esperado que tenha uma implementação da interface IDisposable
+            }
 
-            Console.WriteLine("Press a key to close...");
+            // stack trace é a sequencia de métodos chamados até chegar no disparo da exception.
+
+            Console.WriteLine("\nPress a key to close...");
             Console.ReadKey();
         }
     }
